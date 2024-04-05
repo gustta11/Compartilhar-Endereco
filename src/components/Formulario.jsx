fdbe7ad732b27c182a3a55abb4097700c8e136de
gustta11 <gustavokeven50@gmail.com>
2024-04-05 17:46:17 -0300

    Adicionando hook useForm

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,44 @@
 import './Formulario.css'
-
+import {UseState} from 'react'
+import { useForm} from "react-hook-form"
 
 const Formulario = () =>{
 
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = (e)=>{
+        let botao = document.querySelector('.botaoWhats')
+        let url = `https://api.whatsapp.com/send?phone=${encodeURIComponent(e.contato)}&text=${encodeURIComponent(e.nome)}`
+        botao.setAttribute('href',url)
+        botao.setAttribute('target', '_blank')
+    }
+
+    /*const whats =(e)=>{
+        let botao = document.querySelector('.botaoWhats')
+        let url = `https://api.whatsapp.com/send?phone=${encodeURIComponent(e.contato)}&text=${encodeURIComponent(e.nome)}`
+        botao.setAttribute('href',url)
+        botao.setAttribute('target', '_blank')
+    }*/
 
     return(
         <div className='layoutForm'>
             <div className='conteudoForm'>
-                <p>NOME:<input type="text" /></p>
-                <p>TELEFONE:<input type="text" /></p>
-                <p>CEP:<input type="text" /></p>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    Nome:
+                    <input type="text" {...register('nome')}/>
+                </label>
+                <label >
+                    Contato:
+                    <input type="text" {...register('contato')}/>
+                </label>
+                <label>
+                    CEP:
+                    <input type="text" />
+                </label>
+                <button type='submit'> Enviar </button>
+                <a className='botaoWhats'><button>Whatsapp</button></a>
+              </form>
             </div>
         </div>
     )
